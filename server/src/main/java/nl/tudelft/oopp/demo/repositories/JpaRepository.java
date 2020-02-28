490b2005c8de30067aa4ad7c27e8ec097dcc7779
Mylene Brown-Coleman <m.j.c.brown-coleman@student.tudelft.nl>
2020-02-28 19:50:40 +0100

    added repository classes

--- server/src/main/java/nl/tudelft/oopp/demo/repositories/JpaRepository.java
+++ server/src/main/java/nl/tudelft/oopp/demo/repositories/JpaRepository.java
@@ -1,4 +1,12 @@
 package nl.tudelft.oopp.demo.repositories;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface OOPProject30 extends JpaRepository<{Repository}, {int}>{
+
+@Repository
+public interface ProjectRepository extends JpaRepository<Repository, Integer> {
+
+    // This is a custom query to find a revervation
+    List<Repository> findByTitleContainingOrContentContaining(String text, String textAgain);
+
 }
