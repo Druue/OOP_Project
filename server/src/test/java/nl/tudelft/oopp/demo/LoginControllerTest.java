e8281507226ac26a884b3260c2ab0ff10d723993
Dimitar Antonov Barantiev <D.A.Barantiev@student.tudelft.nl>
2020-02-29 13:30:07 +0100

    Change the test for the LoginController.java so that it uses the Spring API.

--- server/src/test/java/nl/tudelft/oopp/demo/LoginControllerTest.java
+++ server/src/test/java/nl/tudelft/oopp/demo/LoginControllerTest.java
@@ -1,31 +1,55 @@
 package nl.tudelft.oopp.demo;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import nl.tudelft.oopp.demo.controllers.LoginController;
 import nl.tudelft.oopp.demo.models.LoginDetails;
+import nl.tudelft.oopp.demo.repositories.QuoteRepository;
 import nl.tudelft.oopp.demo.services.LoginService;
-import org.junit.jupiter.api.*;
-import org.mockito.Mockito;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import javax.naming.AuthenticationException;
 import javax.servlet.http.HttpSession;
 
 import static org.mockito.BDDMockito.given;
 
-
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = LoginController.class)
 public class LoginControllerTest {
 
-    LoginDetails details = new LoginDetails("Mitko" , "123");
-    private LoginController loginController;
+    @Autowired
+    MockMvc mockMvc;
 
-    private LoginService successService = Mockito.mock(LoginService.class);
-    private LoginService failiingService = Mockito.mock(LoginService.class);
-    private HttpSession session = Mockito.mock(HttpSession.class);
+    @Autowired
+    ObjectMapper mapper;
+
+    LoginDetails details = new LoginDetails("Mitko" , "123");
+
+    @MockBean
+    private QuoteRepository repository;
+
+    @MockBean
+    private LoginService service;
+
+    @Autowired
+    LoginController loginController;
+
+    @Mock
+    HttpSession session;
 
     @Test
     void testSuccessfulLogin() {
-
-        loginController = new LoginController(successService);
 
         ResponseEntity responseEntity = loginController.validateAuthentication(details , session);
 
@@ -34,17 +58,21 @@ public class LoginControllerTest {
     }
 
     @Test
-    void testFailiingService() throws AuthenticationException {
+    void testFailingService() throws AuthenticationException {
 
-        given(failiingService.userValidate(details.getNetID() , details.getPassword())).willThrow(AuthenticationException.class);
-        loginController = new LoginController(failiingService);
+        given(service.userValidate(details.getNetID() , details.getPassword())).willThrow(AuthenticationException.class);
+        loginController = new LoginController(service);
 
         Assertions.assertThrows(AuthenticationException.class , () -> {
-            failiingService.userValidate(details.getNetID() , details.getPassword());
+            service.userValidate(details.getNetID() , details.getPassword());
         });
 
         ResponseEntity responseEntity = loginController.validateAuthentication(details , session);
 
         Assertions.assertEquals(ResponseEntity.badRequest().header("Reason" , "No such user").build() , responseEntity);
+    }
+
+    @Test
+    void testParsingJSONIntoJavaObject() {
     }
 }
