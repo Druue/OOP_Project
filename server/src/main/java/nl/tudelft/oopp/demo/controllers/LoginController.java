3769137b385a53197bacf16ddff0fc3af3904611
Thomas Koppelaar <t.j.koppelaar@student.tudelft.nl>
2020-03-01 14:11:47 +0100

    Changed login controller to use ServerResponse.java

--- server/src/main/java/nl/tudelft/oopp/demo/controllers/LoginController.java
+++ server/src/main/java/nl/tudelft/oopp/demo/controllers/LoginController.java
@@ -1,18 +1,18 @@
 package nl.tudelft.oopp.demo.controllers;
 
+import javax.naming.AuthenticationException;
+import javax.servlet.http.HttpSession;
+
+import nl.tudelft.oopp.demo.models.LoginDetails;
+import nl.tudelft.oopp.demo.models.ServerResponse;
 import nl.tudelft.oopp.demo.services.LoggerService;
 import nl.tudelft.oopp.demo.services.LoginService;
-import nl.tudelft.oopp.demo.models.LoginDetails;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.naming.AuthenticationException;
-import javax.servlet.http.HttpSession;
-
 
 // This class contains the method to answer to a login request from the client
 // It receives as input the provided by the user NetID and a hashed password.
@@ -30,37 +30,35 @@ public class LoginController {
      * This method logs in a user to the application with provided
      * NetID and password. It calls the method userValidate() of the LoginService class to
      * query the database for the user.
-     * @param providedDetails - The provided by the user details mapped to a
-     *                          LoginDetails object through the @RequestBody annotation
+     * @param providedDetails - The provided by the user details mapped to a LoginDetails object
+     *                        through the @RequestBody annotation.
      * @param newUserSession  - The user session, which is automatically created by the Spring Session module
-     * @return An instance of ResponseEntity<String> with status code 200 if the user is successfully authenticated.
-     * Otherwise returns Bad Request response.
+     * @return An instance of ResponseEntity with status code 200 if the user is successfully authenticated.
+     *              Otherwise returns Bad Request response.
      */
-    @PostMapping("/login")
-    public ResponseEntity<String> validateAuthentication(@RequestBody LoginDetails providedDetails,
-                                                              HttpSession newUserSession) {
-
-        String NetID = providedDetails.getNetID();
-        String password = providedDetails.getPassword();
+    @PostMapping(value = "login", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<ServerResponse> validateAuthentication(@RequestBody LoginDetails providedDetails,
+                                                                 HttpSession newUserSession) {
 
         try {
-            String role = service.userValidate(NetID , password);
+            String role = service.userValidate(providedDetails);
+            LoggerService.info(LoginController.class, "User successfully authenticated.");
 
-            LoggerService.info(LoginController.class , "User successfully authenticated with NetID: "
-                    + NetID + " and role: " + role);
-            newUserSession.setAttribute("NetID" , NetID);
-            newUserSession.setAttribute("Role" , role);
+            //TODO: Work with sessions
+            newUserSession.setAttribute("NetID", providedDetails.getNetID());
+            newUserSession.setAttribute("Role", role);
 
-            return ResponseEntity.status(200).build();
+            // Send a response containing a success message, and the user's role.
+            ServerResponse a = new ServerResponse("Successful login!", "CONFIRMATION");
+            return ResponseEntity.ok().body(a);
+        } catch (AuthenticationException e) {
+            LoggerService.info(LoginController.class,"Authentication failed for user with NetID: "
+                                                     + providedDetails.getNetID() + " and password " + providedDetails.getPassword()
+                                                     + " : No such user registered.");
+
+            // Send a response containing an error message.
+            ServerResponse a = new ServerResponse("Invalid user/password combination.", "ERROR");
+            return ResponseEntity.badRequest().body(a);
         }
-        catch (AuthenticationException e) {
-
-           LoggerService.info(LoginController.class ,"Authentication failed for user with NetID: "
-                   + NetID + " and password " + password + " : No such user registered." );
-
-           return ResponseEntity.badRequest().header("Reason" , "No such user").build();
-        }
-
     }
-
 }
---
+++ server/src/main/java/nl/tudelft/oopp/demo/models/ServerResponse.java
@@ -0,0 +1,32 @@
+package nl.tudelft.oopp.demo.models;
+
+public class ServerResponse {
+    String message;
+    String alertType;
+    /**
+     * A Response object sent after a login attempt by a user.
+     * @param message the message that should be sent back.
+     * @param alertType the type of alert that should be used.
+     */
+    public ServerResponse(String message, String alertType) {
+        this.message = message;
+        this.alertType = alertType;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getAlertType() {
+        return alertType;
+    }
+
+    public void setAlertType(String alertType) {
+        this.alertType = alertType;
+    }
+
+}
