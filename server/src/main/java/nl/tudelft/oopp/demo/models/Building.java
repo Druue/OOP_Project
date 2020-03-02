5319193e548120b8ba8464434601d51f05cdd8b7
Mylene Brown-Coleman <m.j.c.brown-coleman@student.tudelft.nl>
2020-03-02 16:17:06 +0100

    adding the building entity/model file

---
+++ server/src/main/java/nl/tudelft/oopp/demo/models/Building.java
@@ -0,0 +1,72 @@
+package nl.tudelft.oopp.demo.models;
+
+import java.util.Collection;
+import java.util.Map;
+import javax.persistence.CascadeType;
+import javax.persistence.CollectionTable;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.MapKeyColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import org.hibernate.annotations.ManyToAny;
+
+/**
+ * Building.
+ */
+@Entity
+@Table(name = "building")
+public class Building {
+
+    /**
+     * Initialises a new instance of {@link Building}.
+     */
+    public Building() {
+    }
+
+    /**
+     * The building's campus number.
+     */
+    @Id
+    @Column(name = "number")
+    public int number;
+
+    /**
+     * The name of the building.
+     */
+    @Column(name = "name")
+    public String name;
+
+    /**
+     * The hours during which the building is open during the week.
+     */
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "openinghours", referencedColumnName = "id")
+    public TimeSlot openingHours;
+
+    /**
+     * OPTIONAL: The foodcourt within the building.
+     */
+    @OneToOne
+    @JoinColumn(name = "foodcourt", referencedColumnName = "building_number")
+    public Foodcourt foodCourt;
+
+    // /**
+    // * A map of available reservation slots for each reservable entity at/in the
+    // building.
+    // */
+    // @OneToMany(mappedBy = "building", cascade = CascadeType.ALL, orphanRemoval =
+    // true)
+    // @CollectionTable(name = "available_reserverations")
+    // @MapKeyColumn(name = "reservable")
+    // Map<Reservable, TimeSlots> availableReservations;
+
+    @ElementCollection
+    @Column(name = "availablereservables")
+    Collection<Reservable> reservables;
+}
