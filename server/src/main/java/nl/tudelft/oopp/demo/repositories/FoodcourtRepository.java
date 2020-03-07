909207d591e0b62708dfa4e7d78bd79decdea097
Mylene Brown-Coleman <m.j.c.brown-coleman@student.tudelft.nl>
2020-03-07 23:21:09 +0100

    changed foodcourt repo

--- server/src/main/java/nl/tudelft/oopp/demo/repositories/FoodcourtRepository.java
+++ server/src/main/java/nl/tudelft/oopp/demo/repositories/FoodcourtRepository.java
@@ -1,21 +1,10 @@
 package nl.tudelft.oopp.demo.repositories;
 
 import nl.tudelft.oopp.demo.models.Foodcourt;
-<<<<<<< HEAD
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface FoodCourtRepository extends JpaRepository<Foodcourt, Long> {
-=======
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-/**
- * interface to implement crud operations for food court.
- */
-@Repository
-public interface FoodcourtRepository extends CrudRepository<Foodcourt, Integer> {
->>>>>>> f84d993b60db840bb42d2351cec23fb3c20cce1a
+public interface FoodCourtRepository extends JpaRepository<Foodcourt, Long>{
 }
