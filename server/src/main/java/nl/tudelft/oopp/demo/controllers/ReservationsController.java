f84d6b86e03dc09b7007b4ec9a67166ffdf3e85f
Dimitar Antonov Barantiev <D.A.Barantiev@student.tudelft.nl>
2020-03-08 13:26:28 +0100

    "Refactor ReservationsController.java to not use sessions and to accept as input NetID and role provided as request parameters."

--- server/src/main/java/nl/tudelft/oopp/demo/controllers/ReservationsController.java
+++ server/src/main/java/nl/tudelft/oopp/demo/controllers/ReservationsController.java
@@ -4,8 +4,9 @@ import javax.servlet.http.HttpSession;
 import nl.tudelft.oopp.demo.services.LoggerService;
 import nl.tudelft.oopp.demo.services.ReservationService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -25,29 +26,18 @@ public class ReservationsController {
      * the cookie value NetID to find all the current reservations of a particular user. The result
      * is then sent as a list of reservation objects with information about every one of them.
      */
-    @GetMapping("/")
-    public <T> ResponseEntity<T> requestUserReservations(HttpSession session) {
+    @PutMapping("/")
+    public <T> ResponseEntity<T> requestUserReservations(@RequestParam String netID,
+                                                         @RequestParam String role) {
 
         // Get the user's session and get the NetID attribute stored in it
         // to query the database for the user's reservations.
         // If the user does not have an existing session, the method logs an error message
         // and sends
 
-        try {
-            sessionValidate(session);
-        } catch (IllegalAccessException e) {
-            session.invalidate();
-            LoggerService.error(ReservationsController.class,
-                "Unauthorized attempt to view reservations -"
-                    + " no existing session found for the client.");
-            return ResponseEntity.badRequest().build();
-        }
-
-        String userNetID = (String) session.getAttribute("NetID");
         LoggerService.info(ReservationsController.class,
-            "Client with NetID: " + userNetID + " requested his current reservations.");
+            "Client with NetID: " + netID + " requested his current reservations.");
         return ResponseEntity.ok().build();
-
     }
 
 
