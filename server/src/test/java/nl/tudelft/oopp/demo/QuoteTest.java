bc0589cdc118473ac04facffe96cd13d73d51428
Mylene Brown-Coleman <m.j.c.brown-coleman@student.tudelft.nl>
2020-03-08 16:22:53 +0100

    fixed errors within thr QuoteTest class

--- server/src/test/java/nl/tudelft/oopp/demo/QuoteTest.java
+++ server/src/test/java/nl/tudelft/oopp/demo/QuoteTest.java
@@ -2,7 +2,8 @@ package nl.tudelft.oopp.demo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import nl.tudelft.oopp.demo.models.FoodCourt;
+import nl.tudelft.oopp.demo.entities.Quote;
+import nl.tudelft.oopp.demo.repositories.QuoteRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -16,10 +17,10 @@ public class QuoteTest {
     public void saveAndRetrieveQuote() {
         String quoteText = "Tell me and I forget. Teach me and I remember. Involve me and I learn.";
         String quoteAuthor = "Benjamin Franklin";
-        FoodCourt.Quote quote = new FoodCourt.Quote(1, quoteText, quoteAuthor);
+        Quote quote = new Quote(1, quoteText, quoteAuthor);
         quoteRepository.save(quote);
 
-        FoodCourt.Quote quote2 = quoteRepository.getOne((long) 1);
+        Quote quote2 = quoteRepository.getOne((long) 1);
         assertEquals(quote, quote2);
     }
 }
