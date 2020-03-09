3b3e7428bdf13799bb0b548f4e5223dc33201dd2
bjo <b.jo@student.tudelft.nl>
2020-03-09 10:12:44 +0100

    Add Controller for the homepage and add basic features to the buttons on the homepage

---
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/HomepageController.java
@@ -0,0 +1,58 @@
+package nl.tudelft.oopp.demo.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HomepageController {
+
+    /**
+     * Handles going to the mainScene.
+     *
+     * @param event the scene from where the function was called.
+     */
+    public void goToMain(ActionEvent event) {
+        try {
+            Parent mainParent = FXMLLoader.load(getClass().getResource("/mainScene.fxml"));
+            Scene mainScene = new Scene(mainParent);
+
+            Stage primaryStage =
+                    (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
+
+            primaryStage.hide();
+            primaryStage.setScene(mainScene);
+            primaryStage.show();
+
+        } catch (IOException e) {
+            System.out.println("IOException in HomepageController");
+        }
+    }
+
+
+    /**
+     * Handles going to the reservation page.
+     *
+     * @param event the scene from where the function was called.
+     */
+    public void goToRes(ActionEvent event) {
+        try {
+            Parent resParent = FXMLLoader.load(getClass().getResource("/reservations.fxml"));
+            Scene resScene = new Scene(resParent);
+
+            Stage primaryStage =
+                    (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
+
+            primaryStage.hide();
+            primaryStage.setScene(resScene);
+            primaryStage.show();
+
+        } catch (IOException e) {
+            System.out.println("IOException in HomepageController");
+        }
+    }
+}
