595d171f5a3ea7356b5de94d30428d1738558d39
Mylene Brown-Coleman <m.j.c.brown-coleman@student.tudelft.nl>
2020-03-12 23:12:21 +0100

    added database config file

---
+++ server/src/main/java/nl/tudelft/oopp/demo/config/UserDataBaseConfig.java
@@ -0,0 +1,35 @@
+package nl.tudelft.oopp.demo.config;
+
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+
+@Configuration
+@EnableJpaRepositories
+@PropertySource("application.properties")
+@EnableTransactionManagement
+public class UserDataBaseConfig {
+    @Autowired
+    private Environment environment;
+
+    /**
+     * Set up the connection to the database.
+     */
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/database_OOPP");
+        dataSource.setUsername("springuser");
+        dataSource.setPassword("krabbypatty");
+
+        return dataSource;
+    }
+}
