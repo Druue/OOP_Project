75ad04c749d9040bcac300094f748db1f9e430b0
Dimitar Antonov Barantiev <D.A.Barantiev@student.tudelft.nl>
2020-03-17 18:43:15 +0100

    "Add methods for finding current reservations by user and reservable in ReservationRepository."

--- server/src/main/java/nl/tudelft/oopp/server/repositories/ReservationRepository.java
+++ server/src/main/java/nl/tudelft/oopp/server/repositories/ReservationRepository.java
@@ -1,8 +1,11 @@
 package nl.tudelft.oopp.server.repositories;
 
+import java.sql.Timestamp;
 import java.util.List;
+import nl.tudelft.oopp.server.models.Reservable;
 import nl.tudelft.oopp.server.models.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -13,6 +16,20 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
 
     List<Reservation> findAllByUser_Id(Long userID);
 
-
     List<Reservation> findAll();
+
+    @Query("select res "
+        + " from Reservation res"
+        + " where res.timeslot.startTime > ?1")
+    List<Reservation> findAllCurrent(Timestamp startSearchTime);
+
+    @Query("select res "
+        + " from Reservation res "
+        + " where res.user.id=?1 and res.timeslot.startTime > ?2")
+    List<Reservation> findAllCurrentByUser(Long userID, Timestamp startSearchTime);
+
+    @Query("select res "
+        + " from Reservation res "
+        + " where res.reservable = ?1 and res.timeslot.startTime > ?2")
+    List<Reservation> findAllCurrentReservable(Reservable reservable, Timestamp timestamp);
 }
