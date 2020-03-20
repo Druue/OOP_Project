194fcf3686d81625bc3377204b1350b5cba15261
Dimitar Antonov Barantiev <D.A.Barantiev@student.tudelft.nl>
2020-03-20 12:47:37 +0100

    DetailsRepository: Add @Repository annotation.

--- server/src/main/java/nl/tudelft/oopp/server/repositories/DetailsRepository.java
+++ server/src/main/java/nl/tudelft/oopp/server/repositories/DetailsRepository.java
@@ -2,7 +2,9 @@ package nl.tudelft.oopp.server.repositories;
 
 import nl.tudelft.oopp.server.models.Details;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DetailsRepository extends JpaRepository<Details, Long> {
 
     boolean existsByName(String name);
