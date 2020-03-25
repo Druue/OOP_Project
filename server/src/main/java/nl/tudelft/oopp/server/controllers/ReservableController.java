699d5874601888b80e6181ce4f81332e66dec504
Dimitar Antonov Barantiev <D.A.Barantiev@student.tudelft.nl>
2020-03-26 00:47:50 +0100

    ReservableController.java: Add logger and AuthorizationService fields.

--- server/src/main/java/nl/tudelft/oopp/server/controllers/ReservableController.java
+++ server/src/main/java/nl/tudelft/oopp/server/controllers/ReservableController.java
@@ -1,10 +1,9 @@
 package nl.tudelft.oopp.server.controllers;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import java.util.ArrayList;
 import java.util.List;
-import javax.management.InstanceAlreadyExistsException;
-import javax.naming.AuthenticationException;
-import javax.persistence.EntityNotFoundException;
 import nl.tudelft.oopp.api.HttpRequestHandler;
 import nl.tudelft.oopp.api.models.ClientRequest;
 import nl.tudelft.oopp.api.models.RoomResponse;
@@ -14,7 +13,6 @@ import nl.tudelft.oopp.server.models.Room;
 import nl.tudelft.oopp.server.services.AuthorizationService;
 import nl.tudelft.oopp.server.services.LoggerService;
 import nl.tudelft.oopp.server.services.ReservableService;
-import nl.tudelft.oopp.server.services.RoomFilteringService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
@@ -24,7 +22,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -32,34 +29,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class ReservableController {
 
     private Logger logger = LoggerFactory.getLogger(ReservableController.class);
-
-    private static final String NOT_ADMIN =
-        "Unauthorized request. The requesting user is not an administrator.";
-
-    private static final String NO_USER_FOUND =
-        "Authentication for user failed. No administrator with that name found.";
+    private Gson gson = new GsonBuilder().serializeNulls().create();
 
     /**
      * Importing the methods from the service class.
      */
     final ReservableService reservableService;
     final AuthorizationService authorizationService;
-    final RoomFilteringService roomService;
 
-    /** Construct a new {@link ReservableController} bean using the following beans.
-     * @param reservableService     The {@link ReservableService} bea to use when fetching, adding
-     *                              and deleting reservables.
-     * @param authorizationService  The {@link AuthorizationService} bean to use when authenticating
-     *                              and authorizing users and administrators.
-     * @param roomService           The {@link RoomFilteringService} bean to use when filtering
-     *                              rooms.
-     */
     public ReservableController(ReservableService reservableService,
-                                AuthorizationService authorizationService,
-                                RoomFilteringService roomService) {
+                                AuthorizationService authorizationService) {
         this.reservableService = reservableService;
         this.authorizationService = authorizationService;
-        this.roomService = roomService;
     }
 
     /**
@@ -87,123 +68,6 @@ public class ReservableController {
         return ResponseEntity.ok(new RoomResponse(responseList));
     }
 
-    /** Receives a GET request for all rooms or bikes of a particular building. First authenticates
-     *      the user by his username using the {@link AuthorizationService} bean and then uses the
-     *      {@link nl.tudelft.oopp.server.services.ReservableService} to fetch all rooms or bikes
-     *      of the building with the provided id as a request parameter. Sends the list wrapped in
-     *      a {@link ResponseEntity} object.
-     *
-     * @param request   The client request containing the username to be authenticated.
-     * @param id        The id of the building to fetch the rooms of.
-     * @param type      The type of reservable to retrieve - rooms or bikes.
-     * @return          A {@link ResponseEntity} object containing a list of the building's rooms.
-     */
-    @GetMapping("/{role:(?:user|admin)}/all/{type}/building")
-    public ResponseEntity<List<Reservable>> getAllReservablesOfBuilding(
-        @RequestBody ClientRequest<String> request,
-        @RequestParam Long id,
-        @PathVariable String type) {
-
-        logger.info("Received GET requests for all " + type
-            + " of building " + id + ". Processing ...");
-
-        try {
-            authorizationService.authenticateUser(request.getUsername());
-        } catch (AuthenticationException e) {
-            logger.error(NO_USER_FOUND);
-            return ResponseEntity.badRequest().build();
-        }
-
-        logger.info("Fetching all + " + type + " of building " + id + " ...");
-
-        List<Reservable> reservablesToSend;
-
-        try {
-            reservablesToSend = reservableService.getAllReservablesForBuilding(id, type);
-        } catch (EntityNotFoundException e) {
-            return ResponseEntity.badRequest().body(null);
-        }
-
-        logger.info("Sending the " + type + " of building " + id +  " ...");
-        return ResponseEntity.ok(reservablesToSend);
-    }
-
-    /** Receives a GET request for all rooms filtered by a certain capacity provided as a request
-     *      parameter. First it authenticates the user via the {@link AuthorizationService} bean,
-     *      then fetches all rooms from the database with the required capacity.
-     * @param request   The {@link ClientRequest} object containing the user username.
-     * @param group     The partition of rooms with respect to the provided capacity.
-     * @param capacity  The provided by the user capacity for filtering.
-     * @return          A {@link ResponseEntity} containing a list of {@link Room} objects.
-     */
-    @GetMapping("/{role:(?:user|admin)}/filter/capacity/{group}")
-    public ResponseEntity<List<Room>> getAllRoomsByFilterCapacity(
-        @RequestBody ClientRequest<Integer> request,
-        @PathVariable String group,
-        @RequestParam Integer capacity) {
-
-        logger.info("Received GET request for all available rooms "
-                + "with capacity " + group + " than " + request.getBody());
-
-        try {
-            authorizationService.authenticateUser(request.getUsername());
-        } catch (AuthenticationException e) {
-            logger.error(NO_USER_FOUND);
-            return ResponseEntity.badRequest().build();
-        }
-
-        List<Room> reservablesToBeSend = roomService.findAllByCapacity(group, capacity);
-
-        logger.info("Sending the filtered by capacity list ...");
-        return ResponseEntity.ok(reservablesToBeSend);
-
-    }
-
-    /** Endpoint that receives a PUT request for adding a new reservable to a building whose
-     *      id is provided as a request parameter and the type of reservable as a path variable.
-     *      The new reservable is encapsulated in a {@link ClientRequest} object.
-     * @param request   The {@link ClientRequest} object containing the new reservable to add.
-     * @param id        The id of the building to add the new reservable to.
-     * @param type      The type of reservable to add - room or bike.
-     * @return          A {@link ResponseEntity} object indicating whether the operation was
-     *                  successful.
-     */
-    @PutMapping("/insert/{type}")
-    public ResponseEntity<ServerResponseAlert> addNewReservable(
-        @RequestBody ClientRequest<Reservable> request,
-        @RequestParam Long id,
-        @PathVariable String type) {
-
-        logger.info("Received PUT request for adding a new " + type + " to building " + id
-            + ". Processing ...");
-
-        try {
-            authorizationService.authenticateUser(request.getUsername());
-        } catch (AuthenticationException e) {
-            logger.error(NO_USER_FOUND);
-            return ResponseEntity.badRequest().build();
-        }
-
-        logger.info("Adding new " + type + " to building " + id);
-
-        try {
-            reservableService.addReservable(request.getBody(), id);
-        } catch (EntityNotFoundException e) {
-            logger.error("Building " + id + " not found!");
-            return ResponseEntity.badRequest().build();
-        } catch (InstanceAlreadyExistsException e) {
-            return ResponseEntity.badRequest().body(new ServerResponseAlert(
-               "Reservable with that name already exists",
-                "ERROR"
-            ));
-        }
-
-        logger.info("Adding of " + type + " to building " + id + " successful. ");
-        return ResponseEntity.ok(new ServerResponseAlert("Adding of " + type
-            + "successful.", "SUCCESS"));
-    }
-
-
     /**
      * Updates a room in the database.
      * @param newReservable the Reservable to replace the old reservable with.
@@ -223,5 +87,20 @@ public class ReservableController {
     public void delete(@PathVariable Long id) {
         reservableService.deleteReservable(id);
     }
+
+    /**
+     * Inserts a new room into the DB.
+     * @param request The room that should be added.
+     * @return A {@link ServerResponseAlert}.
+     */    
+    @PutMapping("/insert/new_room")
+    public ServerResponseAlert addNewRoom(@RequestBody ClientRequest<Room> request) {
+        //TODO: Add authorization process
+
+
+        reservableService.addRoom(request.getBody());
+        return new ServerResponseAlert("Room added", "CONFIRMATION");
+    }
+
 
 }
