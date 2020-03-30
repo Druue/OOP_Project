38265e23f0146f4ead08430a8bea680b95076ace
Thomas Koppelaar <t.j.koppelaar@student.tudelft.nl>
2020-03-30 17:17:48 +0200

    Added unit test folder for api module, moved two to the correct folder on the client-side

---
+++ api/src/test/java/nl/tudelft/oopp/api/UserTest.java
@@ -0,0 +1,13 @@
+package nl.tudelft.oopp.api;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class UserTest {
+    @Test
+    public static void testRoomEntryGeneration() {
+        assertTrue(true);
+    }
+}
---
+++ client/src/test/java/nl/tudelft/oopp/client/controllers/RoomEntryGenerationTest.java
@@ -0,0 +1,13 @@
+package nl.tudelft.oopp.client.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class RoomEntryGenerationTest {
+    @Test
+    public static void testRoomEntryGeneration() {
+        assertTrue(false);
+    }
+}
