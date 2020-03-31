f930eac3759070519f77360cc79c69a926737bb6
mbrowncoleman <m.j.c.brown-coleman@student.tudelft.nl>
2020-03-31 13:51:09 +0200

    gradle does not recognise junit tests

---
+++ server/src/test/java/nl/tudelft/oopp/server/BuildingRequestsControllerTest.java
@@ -0,0 +1,54 @@
+package nl.tudelft.oopp.server;
+
+
+import nl.tudelft.oopp.api.models.ClientRequest;
+import nl.tudelft.oopp.api.models.ServerResponseAlert;
+import nl.tudelft.oopp.server.controllers.BuildingRequestController;
+import nl.tudelft.oopp.server.models.Building;
+import nl.tudelft.oopp.server.repositories.BuildingNumber;
+import nl.tudelft.oopp.server.repositories.BuildingRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = BuildingRequestController.class)
+class BuildingRequestControllerTest {
+
+    @InjectMocks
+    private BuildingRequestController buildingRequestController;
+
+    @Mock
+    BuildingRepository buildingRepository;
+
+    @Test
+    void addBuildings() {
+        MockHttpServletRequest request = new MockHttpServletRequest();
+
+        RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
+        ClientRequest<Building> building = new ClientRequest<Building>();
+
+        when(buildingRepository.getAllBy()).thenReturn((List<BuildingNumber>) building);
+
+
+        ResponseEntity<ServerResponseAlert> responseEntity = buildingRequestController.addBuilding(building);
+
+        assertThat(responseEntity.getStatusCodeValue()).isEqualTo(200);
+
+    }
+
+}
+
