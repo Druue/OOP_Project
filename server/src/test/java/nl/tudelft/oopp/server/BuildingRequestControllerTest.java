591989e62d34a9cc98deb834f67b1c16719044ed
mbrowncoleman <m.j.c.brown-coleman@student.tudelft.nl>
2020-04-01 22:28:18 +0200

    #120 added more test cases to the BuildingRequestController however there are still some gradle issues.

--- server/src/test/java/nl/tudelft/oopp/server/BuildingRequestControllerTest.java
+++ server/src/test/java/nl/tudelft/oopp/server/BuildingRequestControllerTest.java
@@ -30,6 +30,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
@@ -37,6 +38,7 @@ import org.springframework.test.context.TestContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -64,6 +66,7 @@ class BuildingRequestControllerTest {
     Food food;
     Building mockBuilding1;
     Building mockBuilding2;
+
     /**
      * This is the main entry point for server side tests
      * it will perform a request and return a type that
@@ -85,6 +88,7 @@ class BuildingRequestControllerTest {
         mockMvc = MockMvcBuilders
                 .standaloneSetup(buildingRequestController)
                 .build();
+
         openingTime = Timestamp.valueOf("2020-02-04 09:30:00");
         closingTime = Timestamp.valueOf("2020-04-09 22:30:00");
         timeSlot = new TimeSlot(39832L, openingTime, closingTime);
@@ -106,12 +110,41 @@ class BuildingRequestControllerTest {
 
     @Test
     public void sendAllBuildingsSuccess() throws Exception {
+        System.out.println(mockBuilding1.toString());
         List<Building> buildings = Arrays.asList(
                 mockBuilding1,
                 mockBuilding2);
         Mockito.when(buildingServiceMock.getAllBuildings()).thenReturn(buildings);
-        mockMvc.perform(get("/buildings"))
-                .andExpect(status().isOk())
+        mockMvc.perform(get("/buildings/admin/all")
+                .accept(MediaType.parseMediaType("application/json;charset=UTF-8")))
+                .andExpect(status().is(200))
+                .andExpect(content().contentType("application/json;charset=UTF-8"))
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect((ResultMatcher) jsonPath("$[0].number", is(36)))
+                .andExpect((ResultMatcher) jsonPath("$[0].details", is(4)))
+                .andExpect((ResultMatcher) jsonPath("$[0].foodcourt", is(1)))
+                .andExpect((ResultMatcher) jsonPath("$[0].openingHours", is(22)))
+                .andExpect((ResultMatcher) jsonPath("$[0].availableTimeslots", is(7)))
+                .andExpect((ResultMatcher) jsonPath("$[1].number", is(21)))
+                .andExpect((ResultMatcher) jsonPath("$[1].details", is(4)))
+                .andExpect((ResultMatcher) jsonPath("$[1].foodcourt", is(0)))
+                .andExpect((ResultMatcher) jsonPath("$[1].openingHours", is(12)))
+                .andExpect((ResultMatcher) jsonPath("$[1].availableTimeslots", is(8)));
+        Mockito.verify(buildingServiceMock, Mockito.times(1)).getAllBuildings();
+        Mockito.verifyNoMoreInteractions(buildingServiceMock);
+
+    }
+
+
+    @Test
+    public void sendAllBuildingsFail() throws Exception {
+        List<Building> buildings = Arrays.asList(
+                mockBuilding2,
+                mockBuilding1);
+
+        Mockito.when(buildingServiceMock.getAllBuildings()).thenReturn(buildings);
+        mockMvc.perform(get("buildings/user/all"))
+                .andExpect(status().is(404))
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
                 .andExpect(jsonPath("$", hasSize(2)))
                 .andExpect((ResultMatcher) jsonPath("$[0].number", is(36)))
@@ -129,6 +162,19 @@ class BuildingRequestControllerTest {
 
     }
 
+    @Test
+    public void sendAllBuildingsInfoSuccess() throws Exception {
+        List<Building> buildings = Arrays.asList(
+                mockBuilding2,
+                mockBuilding1);
+        Mockito.when(buildingServiceMock.getAllBuildings()).thenReturn(buildings);
+        mockMvc.perform(get("/buildings/user/all/information")
+                .accept(MediaType.parseMediaType("application/json;charset=UTF-8")))
+                .andExpect(status().is(200))
+                .andExpect(MockMvcResultMatchers.content().contentType("application/json;charset=UTF-8"));
+
+
+    }
 
 }
 
