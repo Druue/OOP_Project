e6052e94b94b11e01b270a5b08546c81bd6a0686
bjo <b.jo@student.tudelft.nl>
2020-04-01 09:29:10 +0200

    Add functionality to the menu bar

--- client/src/main/java/nl/tudelft/oopp/client/controllers/AddRoomsController.java
+++ client/src/main/java/nl/tudelft/oopp/client/controllers/AddRoomsController.java
@@ -121,7 +121,51 @@ public class AddRoomsController {
             primaryStage.show();
 
         } catch (IOException e) {
-            System.out.println("IOException in MainSceneController");
+            System.out.println("IOException in AddRoomsController");
+        }
+    }
+
+    /**
+     * Handles going to the add rooms page.
+     *
+     * @param event the scene from where the function was called.
+     */
+    public void goToAddRoom(ActionEvent event) {
+        try {
+            Parent roomParent = FXMLLoader.load(getClass().getResource("/admin-addRoom.fxml"));
+            Scene roomScene = new Scene(roomParent);
+
+            Stage primaryStage =
+                    (Stage) (roomNameInput.getScene().getWindow());
+
+            primaryStage.hide();
+            primaryStage.setScene(roomScene);
+            primaryStage.show();
+
+        } catch (IOException e) {
+            System.out.println("IOException in AddRoomsController");
+        }
+    }
+
+    /**
+     * Handles going to the add buildings page.
+     *
+     * @param event the scene from where the function was called.
+     */
+    public void goToAddBuilding(ActionEvent event) {
+        try {
+            Parent buildingParent = FXMLLoader.load(getClass().getResource("/admin-addBuilding.fxml"));
+            Scene buildingScene = new Scene(buildingParent);
+
+            Stage primaryStage =
+                    (Stage) (roomNameInput.getScene().getWindow());
+
+            primaryStage.hide();
+            primaryStage.setScene(buildingScene);
+            primaryStage.show();
+
+        } catch (IOException e) {
+            System.out.println("IOException in AddRoomsController");
         }
     }
 }
